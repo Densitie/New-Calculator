b2eb4611da35e02b9371a6327667ecf80c510893
arkir7294 <arkir7294@ugcloud.ca>
2019-09-21 16:09:40 -0700

    This is a GUI Average Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ *This is a GUI grade average calculator
+ * Septembre 21 2019
+*/
 
 /**
  *
